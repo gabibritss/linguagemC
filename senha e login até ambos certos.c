9dd338d9c4aca193b2c634ad19a8e4fe37715d91
✧*̥˚ Gabriela *̥˚✧ <144128006+gabibritss@users.noreply.github.com>
2023-10-25 11:28:48 -0300

    Add files via upload

---
+++ senha e login até ambos certos.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+
+    char senhasalva[200] = "gabislinda";
+    char loginsalvo[200] = "gabibritss";
+    char longin[200];
+    char senha[200];
+
+    do
+    {
+        printf("Login:");
+        printf("Senha:");
+
+        if
+        {
+            (login = longinsalvo &&senha = senhasalva);
+            printf("Acesso aceito");
+         }
+         else
+         {
+            printf("Acesso negado");
+        }
+    } while
+
+        return 0;
+}
